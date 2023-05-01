689f06622468c58e1bd496476b307680b28ad9a9
GoX2Maker <gox2maker@gmail.com>
2023-05-01 11:23:56 +0900

    feat: Define print_Hello()
    print hello to all names in names.

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 # Tset python env
-print('hello')
+def print_Hello():
+    animals = ['dog', 'cat', 'hamster']
+    food = [
+	'chiken',
+	'pizza',
+	'hamberger'
+    ]
+    names = [
+	'John',
+	'Jane',
+	'Gil-dong',
+    ]
+    for name in names:
+        print(f'hello, {name}')
+
+if __name__ == '__main__':
+    print_Hello()
+
